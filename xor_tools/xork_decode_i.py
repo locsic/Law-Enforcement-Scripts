4a718a1a61cb947056ac071005d4e79f738393bc
Loc <locvnguy@gmail.com>
2012-08-12 12:23:31 -0400

    Improved xor_decode to add an ability to ignore certain hex values

--- xor_tools/xork_decode_i.py
+++ xor_tools/xork_decode_i.py
@@ -1,4 +1,4 @@
-import sys
+import sys,binascii
 #@Title:        XOR File
 #@Date:         08/06/12
 #@Version:      .1
@@ -7,16 +7,22 @@ import sys
 #               MASK: 0-255
 #@TODO:         
 #
+key = ""
+ignorek = ""
 def decode_filelist(*arg):
     for x in range(len(arg[0])):
       b = bytearray(open(arg[0][x], 'rb').read())
       for i in range(len(b)):
-        b[i] ^= sys.argv[1]
+        if not b[i] == ord(ignorek):
+          b[i] ^= binascii.unhexlify(sys.argv[1])
+        #else: we don't do anything
       #Assuming the filename has a dot
       open(arg[0][x].split('.')[0]+".xcoded",'wb').write(b)
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
-        print('Usage: %s [MASK] [FILE ...]' % sys.argv[0])
+        print('Usage: %s [MASK] [IGNORE] [FILE ...]' % sys.argv[0])
         sys.exit(1)
+    key = binascii.unhexlify(sys.argv[1])
+    ignorek = binascii.unhexlify(sys.argv[2])
     decode_filelist(sys.argv[2:])
